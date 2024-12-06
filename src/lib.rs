030c1152d9d150275236e0f23c0f352a78630561
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-12-05 23:19:36 -0500

    Remove debug

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,6 @@ macro_rules! get_plugin {
         $plugins
             .get(stringify!($name))
             .map(|p| &p.instance as &dyn $name::PluginAPI)
-            //.expect(&format!("Plugin {} not found", stringify!($name)))
-            dbg!($name)
+            .expect(&format!("Plugin {} not found", stringify!($name)))
     };
 }
